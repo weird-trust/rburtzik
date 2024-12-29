5585b55d0c858c97345463ab6382569916f42763
Robert <83475454+weird-trust@users.noreply.github.com>
2024-12-29 23:01:40 +0100

    Delete .vercel directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/demo/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.DDJXfJx8.js","_app/immutable/chunks/legacy.H3Rk6sGl.js","_app/immutable/chunks/runtime.CNqDqZH_.js","_app/immutable/chunks/index.BW-CVLzd.js","_app/immutable/chunks/lifecycle.N_inlhDP.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/6.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 6;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/photo/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/6.tKNvWbKT.js","_app/immutable/chunks/legacy.H3Rk6sGl.js","_app/immutable/chunks/runtime.CNqDqZH_.js","_app/immutable/chunks/each.HYey5AK_.js","_app/immutable/chunks/index.BW-CVLzd.js","_app/immutable/chunks/lifecycle.N_inlhDP.js","_app/immutable/chunks/index-client.Dqq7yuzS.js"];
-export const stylesheets = ["_app/immutable/assets/6.Cq5dZcRa.css"];
-export const fonts = [];
