b9e1cba043a344ed691c305dcefdd0f285e3341b
Robert <83475454+weird-trust@users.noreply.github.com>
2025-01-01 21:18:26 +0100

    Delete .vercel directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.u915sFbD.js","_app/immutable/chunks/legacy.H3Rk6sGl.js","_app/immutable/chunks/runtime.CNqDqZH_.js","_app/immutable/chunks/i18n.BhVdZpSG.js","_app/immutable/chunks/entry.ByoblpRL.js","_app/immutable/chunks/utils.Bb3n8h1z.js","_app/immutable/chunks/control.CYgJF_JY.js","_app/immutable/chunks/index-client.Dqq7yuzS.js","_app/immutable/chunks/svelte-head.Byx9516y.js","_app/immutable/chunks/if.BVymDcXa.js","_app/immutable/chunks/index.CIucM2I2.js","_app/immutable/chunks/lifecycle.N_inlhDP.js","_app/immutable/chunks/props.BTy4-oo2.js","_app/immutable/chunks/store.B5jdUjFW.js","_app/immutable/chunks/index.BW-CVLzd.js","_app/immutable/chunks/each.HYey5AK_.js"];
-export const stylesheets = ["_app/immutable/assets/0.DNTDnauU.css"];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/5.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 5;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/demo/paraglide/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/5.P63cgLb-.js","_app/immutable/chunks/legacy.H3Rk6sGl.js","_app/immutable/chunks/runtime.CNqDqZH_.js","_app/immutable/chunks/render.B2tnZ6U6.js","_app/immutable/chunks/svelte-head.Byx9516y.js","_app/immutable/chunks/lifecycle.N_inlhDP.js","_app/immutable/chunks/store.B5jdUjFW.js","_app/immutable/chunks/utils.Bb3n8h1z.js","_app/immutable/chunks/i18n.BhVdZpSG.js","_app/immutable/chunks/entry.ByoblpRL.js","_app/immutable/chunks/control.CYgJF_JY.js","_app/immutable/chunks/index-client.Dqq7yuzS.js"];
-export const stylesheets = [];
-export const fonts = [];
